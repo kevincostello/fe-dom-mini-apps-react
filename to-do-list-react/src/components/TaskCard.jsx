9fc17d93bf92901e5eb67c7199d4a0140b87fc74
Kevin Costello <kevin_m_costello@hotmail.com>
2020-03-02 15:01:15 +0000

    Solved adding new task to state

--- to-do-list-react/src/components/TaskCard.jsx
+++ to-do-list-react/src/components/TaskCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 function TaskCard(props) {
-  console.log(props);
   return (
     <main>
       <li>
--- to-do-list-react/src/components/Tasks.jsx
+++ to-do-list-react/src/components/Tasks.jsx
@@ -16,34 +16,67 @@ export class Tasks extends Component {
           <>
             <ul>
               <TaskList tasks={this.state.tasks} />
-              {console.log(
+              {/* {console.log(
                 "maximumj id is: ",
                 this.state.tasks.reduce((a, c) => {
                   return Math.max(a.task_id, c.task_id);
                 })
-              )}
+              )} */}
             </ul>
 
-            <form action="">
+            <form action="" onSubmit={this.handleSubmit}>
               <label htmlFor="">
-                <input type="text"></input>
+                Enter task name:
+                <input
+                  type="text"
+                  name="task_name"
+                  onChange={this.handleChange}
+                ></input>
               </label>
+              <label htmlFor="">
+                Due date:
+                <input
+                  type="date"
+                  id=""
+                  name="due_date"
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button>Submit</button>
             </form>
           </>
         )}
       </main>
     );
   }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    console.log(name + value);
+    this.setState(currentState => {
+      return { newTask: { ...currentState.newTask, [name]: value } };
+    });
+    console.log("Tasks are:", this.state.newTask);
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(event);
+    this.setState(currentState => {
+      return { tasks: [...currentState.tasks, currentState.newTask] };
+    });
+  };
+
   componentDidMount() {
     this.setState({
       tasks: [
         {
-          task_id: 10,
+          // task_id: 10,
           task_name: "Tidy sock drawer",
           due_date: new Date("2022-02-05").toISOString()
         },
         {
-          task_id: 2,
+          // task_id: 2,
           task_name: "Sort stamp collection",
           due_date: new Date("2020-03-05").toISOString()
         }
@@ -53,9 +86,9 @@ export class Tasks extends Component {
   }
 
   componentDidUpdate(props, prevProps) {
-    console.log("the length is", this.state.tasks);
-    api.autoIncrementID(this.state.tasks);
-    console.log("the tasks will be:", props, prevProps);
+    // console.log("the length is", this.state.tasks);
+    // api.autoIncrementID(this.state.tasks);
+    // console.log("the tasks will be:", props, prevProps);
   }
 }
 
